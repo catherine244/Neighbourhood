74cfa1955be7117f8e2b3e73a148b1ba3a23d19f
catherine244 <ccayreen24@gmail.com>
2020-02-23 11:57:00 +0300

    created class BlogPost   in models.py

--- watch/models.py
+++ watch/models.py
@@ -102,4 +102,23 @@ class Profile(models.Model):
     neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name         
+        return self.name   
+    
+    
+    
+class BlogPost(models.Model):
+    title = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='post/')
+    post = HTMLField()
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    post_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    @classmethod
+    def search_blogpost(cls, search_term):
+        blogs = cls.objects.filter(Q (username__username=search_term) | Q (neighbourhood__neighbourhood=search_term) | Q (title__icontains=search_term))
+        return blogs
+          
