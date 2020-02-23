0958a86d67fa638331bef91407b610fb079c562c
catherine244 <ccayreen24@gmail.com>
2020-02-23 21:03:30 +0300

    registered models in admin.py

--- watch/admin.py
+++ watch/admin.py
@@ -1,6 +1,15 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import Neighbourhood, healthservices, notifications, Business, Health, Authorities, BlogPost, Profile
+
+class HealthAdmin(admin.ModelAdmin):
+    filter_horizontal = ['healthservices']
 
 # Register your models here.
+admin.site.register(Neighbourhood)
+admin.site.register(Health, HealthAdmin)
+admin.site.register(Business)
+admin.site.register(healthservices)
+admin.site.register(Authorities)
+admin.site.register(BlogPost)
+admin.site.register(Profile)
+admin.site.register(notifications)
