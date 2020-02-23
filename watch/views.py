72ec065e8da6dc2a8e89bd30185aa0ce52e9b4f7
catherine244 <ccayreen24@gmail.com>
2020-02-23 12:04:17 +0300

    defined index request     in views.py

--- watch/views.py
+++ watch/views.py
@@ -4,3 +4,14 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    try:
+        if not request.user.is_authenticated:
+            return redirect('/accounts/login/')
+        current_user = request.user
+        profile = Profile.objects.get(username=current_user)
+    except ObjectDoesNotExist:
+        return redirect('create-profile')
+
+    return render(request, 'index.html')
