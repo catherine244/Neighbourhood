8b7b2e1aa9488d1bb52b01c66c14fdee5ea0a789
catherine244 <ccayreen24@gmail.com>
2020-02-23 12:47:04 +0300

    defined new_blogpost  request in views.py

--- watch/views.py
+++ watch/views.py
@@ -128,6 +128,28 @@ def update_profile(request):
         form = ProfileForm()
 
     return render(request, 'update_profile.html', {"form":form}
+                  
+                  
+@login_required(login_url='/accounts/login/')
+def new_blogpost(request):
+    current_user = request.user
+    profile = Profile.objects.get(username=current_user)
+
+    if request.method == 'POST':
+        form  = BlogPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            blogpost = form.save(commit = False)
+            blogpost.username = current_user
+            blogpost.neighbourhood = profile.neighbourhood
+            blogpost.save()
+
+        return HttpResponseRedirect('/blog')
+
+    else:
+        form = BlogPostForm()
+
+    return render(request, 'blogpost_form.html', {"form":form})                  
+                  
     
     
     
