b3782b91e36192691262a8ec533f7380156b7d54
catherine244 <ccayreen24@gmail.com>
2020-02-23 11:47:34 +0300

    created class Notifications in models.py

--- watch/models.py
+++ watch/models.py
@@ -25,3 +25,16 @@ class Neighbourhood(models.Model):
     def update_neighbourhood(self, neighbourhood_name):
         self.neighbourhood_name = neighbourhood_name
         self.save()
+        
+        
+class notifications(models.Model):
+    title = models.CharField(max_length=100)
+    notification = HTMLField()
+    priority = models.CharField(max_length=15, choices=Priority, default="Informational")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE
+    )
+    post_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title        
